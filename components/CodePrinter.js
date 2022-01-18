b33da19f9362662d6becc4a9a6b32423f6e8e41f
Husseinkizz <hssnkizz@gmail.com>
2022-01-18 10:36:23 +0300

    Started to work on syntax highlighting for examples

---
+++ components/CodePrinter.js
@@ -0,0 +1,14 @@
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import Prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
+
+let codeString = '(num) => num + 1';
+SyntaxHighlighter.registerLanguage('jsx', jsx);
+
+export const CodePrinter = () => {
+  return (
+    <SyntaxHighlighter language="javascript" style={dark}>
+      {codeString}
+    </SyntaxHighlighter>
+  );
+};
---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import { CodePrinter } from '../components/CodePrinter';
+
+const test = () => {
+  return (
+    <div>
+      <h1>foo!</h1>
+      <CodePrinter />
+    </div>
+  );
+};
+
+export default test;
